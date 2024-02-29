272ee89b9a082e6b62d5e1c4accf9462192b3522
Federico Crespi <crespif@celta.com.ar>
2024-02-29 12:30:28 -0300

    Se setearon cabeceras de endpoint

--- src/app/api/client/[id]/route.tsx
+++ src/app/api/client/[id]/route.tsx
@@ -8,7 +8,10 @@ export async function GET(req: Request, { params } : { params: { id: string }}) 
       {
         status: 200,
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': "application/json",
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         },
       }
     );
