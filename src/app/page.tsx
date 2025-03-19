aa2a417e7819f2881cd27feb8c9066f621493d3c
fede crespi <fdcrespi@gmail.com>
2025-03-19 13:24:04 -0300

    Se controla ingreso de datos invalidos en el input de documento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,13 @@ export default function Home() {
     setDoc(e.target.value);
   };
 
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    // Redirect to dashboard
+    window.location.href = `dashboard/${doc}`;
+  };
+
   return (
     <main className="flex flex-col items-center justify-between p-4">
       <div className="flex flex-col items-center justify-center p-4 bg-gray-50 rounded-lg shadow-lg">
@@ -20,14 +27,25 @@ export default function Home() {
           Cajero Virtual
         </h1>
         <p className="text-gray-600">Bienvenido al Cajero Virtual de CELTA</p>
-        <div className="flex md:flex-row flex-col items-center justify-center p-4">
+        <form onSubmit={handleSubmit} className="flex md:flex-row flex-col items-center justify-center p-4">
           <input
-            type="number"
+            type="text" 
+            pattern="[0-9]*"
             placeholder="Documento"
             className="p-2 m-2 border border-gray-300 rounded-lg"
             onChange={handleChange}
             required
+            inputMode="numeric"
+            title="Formato de documento invalido, ingrese solo numeros"
           />
+          <button
+            type="submit"
+            className="flex items-center gap-2 self-start rounded-lg bg-blue-500 py-2 px-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base m-auto"
+          >
+            <MagnifyingGlassIcon className="w-5" />
+            <span>{loading ? "Cargando..." : "Buscar"}</span>
+          </button>
+         {/*  
           <Link
             href={`dashboard/${doc}`}
             className="flex items-center gap-2 self-start rounded-lg bg-blue-500 py-2 px-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base m-auto"
@@ -35,8 +53,8 @@ export default function Home() {
           >
             <MagnifyingGlassIcon className="w-5" />
             <span>{loading ? "Cargando..." : "Buscar"}</span>
-          </Link>
-        </div>
+          </Link> */}
+        </form>
       </div>
     </main>
   );
