14c063ab2e330b299ecb76a8c2e6a6ff21d5e4d0
fede crespi <fdcrespi@gmail.com>
2025-04-09 08:11:49 -0300

    se da formato a boton de volver

--- src/app/pay/page.tsx
+++ src/app/pay/page.tsx
@@ -92,12 +92,13 @@ export default function PaymentStatus() {
                   d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
                 />
               </svg>
-              <p className="mt-4 text-center">No pudimos procesar tu pago. Error en el pago / Pago cancelado. Por favor, intenta nuevamente.</p>
+              <p className="mt-4 text-center">Error en el pago / pago cancelado.</p>
+              <p className="mt-2 text-center">Por favor, intenta nuevamente.</p>
             </div>
           )}
         </div>
         <div className="flex justify-center gap-4">
-          <button>
+          <button className="bg-gray-200 text-gray-700 py-2 px-4 rounded">
             <Link href="/">Volver al inicio</Link>
           </button>
         </div>
