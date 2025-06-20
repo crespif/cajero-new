23c93d9f273760cb4acfc95cc659ba0421c5bac5
fdcrespi <fdcrespi@gmail.com>
2025-06-20 17:54:46 -0300

    Se crea sitio en mantenimiento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const [doc, setDoc] = useState("10");
@@ -22,40 +23,19 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-between p-4">
-      <div className="flex flex-col items-center justify-center p-4 bg-gray-50 rounded-lg shadow-lg">
+      
         <h1 className="text-3xl font-bold text-center text-gray-800">
-          Cajero Virtual
+          Sitio en mantenimiento
         </h1>
-        <p className="text-gray-600">Bienvenido al Cajero Virtual de CELTA</p>
-        <form onSubmit={handleSubmit} className="flex md:flex-row flex-col items-center justify-center p-4">
-          <input
-            type="text" 
-            pattern="[0-9]*"
-            placeholder="Documento"
-            className="p-2 m-2 border border-gray-300 rounded-lg"
-            onChange={handleChange}
-            required
-            inputMode="numeric"
-            title="Formato de documento invalido, ingrese solo numeros"
-          />
-          <button
-            type="submit"
-            className="flex items-center gap-2 self-start rounded-lg bg-blue-500 py-2 px-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base m-auto"
-          >
-            <MagnifyingGlassIcon className="w-5" />
-            <span>{loading ? "Cargando..." : "Buscar"}</span>
-          </button>
-         {/*  
-          <Link
-            href={`dashboard/${doc}`}
-            className="flex items-center gap-2 self-start rounded-lg bg-blue-500 py-2 px-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base m-auto"
-            onClick={() => setLoading(true)}
-          >
-            <MagnifyingGlassIcon className="w-5" />
-            <span>{loading ? "Cargando..." : "Buscar"}</span>
-          </Link> */}
-        </form>
-      </div>
+        <p className="text-gray-600">disculpe las molestias</p>
+        <Image
+          src="/unnamed.png"
+          alt="Maintenance"
+          width={300}
+          height={200}
+          className="mt-4 rounded-lg"
+        />
+      
     </main>
   );
 }
