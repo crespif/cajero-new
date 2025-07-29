17eb40418321465c140897e0535cb3346acfaf68
fede crespi <fdcrespi@gmail.com>
2025-07-29 09:19:03 -0300

    Se cambia el mensaje de que la factura ya tiene un pago

--- src/app/ui/dialog.tsx
+++ src/app/ui/dialog.tsx
@@ -1,37 +1,66 @@
-export default function Dialog({ open, setOpen }: { open: boolean; setOpen: (open: boolean) => void }) {
+export default function Dialog({
+  open,
+  setOpen,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) {
   return (
     open && (
-    <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="fixed z-10 inset-0 overflow-y-auto">
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+          <div className="fixed inset-0 transition-opacity" aria-hidden="true">
             <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-            </div>
-            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+          </div>
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
             <div>
-                <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+              <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
                 {/* svg alerta */}
-                <svg className="h-6 w-6 text-green-600" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                </div>
-                <div className="mt-3 text-center sm:mt-5">
-                <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                    Factura pagada
+                <svg
+                  className="h-6 w-6 text-yellow-500"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M12 8v4l3 3" />
+                  <circle cx="12" cy="12" r="10" />
+                </svg>
+              </div>
+              <div className="mt-3 text-center sm:mt-5">
+                <h3
+                  className="text-lg leading-6 font-medium text-gray-900"
+                  id="modal-title"
+                >
+                  Proceso de pago en curso
                 </h3>
                 <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Ya existe un pago por esta factura, el mismo se verá reflejado en su cuenta corriente dentro de las 24hs.
-                    </p>
+                  <p className="text-sm text-gray-500">
+                    Se ha iniciado un proceso de pago para esta factura. Una vez confirmado, el movimiento se reflejará en su cuenta corriente dentro de las próximas 24 horas.
+                  </p>
                 </div>
-                </div>
+              </div>
             </div>
             <div className="mt-5 sm:mt-6">
-                <button type="button" onClick={() => setOpen(false)} className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:text-sm">
+              <button
+                type="button"
+                onClick={() => setOpen(false)}
+                className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:text-sm"
+              >
                 De acuerdo
-                </button>
+              </button>
             </div>
-            </div>
+          </div>
         </div>
-    </div>
+      </div>
     )
-  )
+  );
 }
