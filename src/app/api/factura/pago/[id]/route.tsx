e2a8bd906c97a9bc3c3155d7c0b4e63886282530
fede crespi <fdcrespi@gmail.com>
2025-07-31 10:20:22 -0300

    Se ve error por consola

--- src/app/api/factura/pago/[id]/route.tsx
+++ src/app/api/factura/pago/[id]/route.tsx
@@ -1,12 +1,12 @@
 export async function GET(request: Request, { params }: { params: { id: string } }) {
     const id = params.id;
     try {
-        console.log(id);
         const query = await fetch(`http://200.45.235.121:3000/payments/${id}`);
         //const query = await fetch(`http://localhost:3000/payments/${id}`);
         const data = await query.json();
         return new Response(JSON.stringify(data), { status: 200 });
     } catch (error) {
+        console.error("Error fetching payment:", error);
         return new Response(JSON.stringify({ error: "Error al buscar el pago" }), { status: 500 });
     }
 }
--- src/app/dashboard/list.tsx
+++ src/app/dashboard/list.tsx
@@ -34,6 +34,7 @@ export default function ListInvoice({
     const query = await fetch(`/api/factura/pago/${invoice.FacturaID}`);
     const data = await query.json();
     if (!data.error) {
+      console.log(data);
       setLoading(false);
       setOpen(true);
       return;
