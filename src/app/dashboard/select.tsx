a9c4df5d221e577573b0d303ca043a34306b74b8
crespif <crespif@celta.com.ar>
2026-07-24 08:15:36 -0300

    se excluye mes 9

--- src/app/dashboard/select.tsx
+++ src/app/dashboard/select.tsx
@@ -14,7 +14,7 @@ export default function SelectSumin({
   facturas: Factura[];
 }) {
   const [client, setClient] = useState(clientes[0]);
-  const monthExclude = [8];
+  const monthExclude = [9];
 
   const [facts, setFacts] = useState(
     facturas.filter(
